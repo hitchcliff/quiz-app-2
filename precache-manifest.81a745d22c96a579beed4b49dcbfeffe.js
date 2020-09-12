ac9a49eeebdd197e6bf796f7eb9bc388bc261335
Kevin Nacario <hitchcliff1@gmail.com>
2020-09-12 17:26:26 +0800

    Updates

---
+++ precache-manifest.81a745d22c96a579beed4b49dcbfeffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e05fa438fc713b16c572256d8f6c7de",
+    "url": "/quiz-app-2/index.html"
+  },
+  {
+    "revision": "5e0ac7eb153eb0fa73d3",
+    "url": "/quiz-app-2/static/css/main.9e8586ad.chunk.css"
+  },
+  {
+    "revision": "300b6fa79cc020bd757a",
+    "url": "/quiz-app-2/static/js/2.2743dc90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app-2/static/js/2.2743dc90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0ac7eb153eb0fa73d3",
+    "url": "/quiz-app-2/static/js/main.048451d0.chunk.js"
+  },
+  {
+    "revision": "2169bb99c09debf4375d",
+    "url": "/quiz-app-2/static/js/runtime-main.d6daec7a.js"
+  },
+  {
+    "revision": "76e11f1bda55034602c2bc0aa3b02e34",
+    "url": "/quiz-app-2/static/media/question-mark.76e11f1b.svg"
+  }
+]);
